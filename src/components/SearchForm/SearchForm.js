be3149845c4e79a071f6e8ec64f7da865462544c
Anastasia Bobrenko <Bobrenko88@yandex.ru>
2023-07-29 21:23:46 +0600

    feat: добавить функции обработки данных инпутов, убрать Novalidate из SearchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,18 +1,33 @@
 import './SearchForm.css'
 import '../App/App.css'
+import { useState } from "react"
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox'
 
 function SearchForm({ isSmallScreen }) {
+  const [formValues, setFormValues] = useState({
+    nameRU: '',
+  })
+
+  function handleChange(e) {
+    const input = e.target
+    setFormValues({
+      ...formValues,
+      [input.name]: input.value
+    })
+  }
+
   return (
     <section className='search-form'>
-      <form className='search-form__form' noValidate>
+      <form className='search-form__form'>
         {!isSmallScreen && <div className='search-form__search-icon' />}
         <input
           type="text"
+          value={formValues.nameRu}
           className="search-form__input"
           name='movie'
           id='movie-input'
           placeholder="Фильм"
+          onChange={handleChange}
           minLength="1"
           maxLength="40"
           required />
