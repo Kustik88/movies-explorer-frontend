c99ed04ac66062a5844d20819eccf668aeaa909d
Anastasia Bobrenko <Bobrenko88@yandex.ru>
2023-08-25 21:44:22 +0700

    feat: исправить адрес юзер эпай на локалхост

--- src/constants/baseUrl.js
+++ src/constants/baseUrl.js
@@ -1,3 +1,3 @@
-export const BASE_URL_USER_API = 'https://kust.nomoreparties.sbs'
+export const BASE_URL_USER_API = 'http://localhost:3000/'
 
 export const BASE_URL_MOVIES_API = 'https://api.nomoreparties.co'
